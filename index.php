0bb96283ff71a43b78035123acaef81f7705de4c
miv-dev <i@mike39.ru>
2023-02-21 14:19:11 +0300

    Задание 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<header style="display: flex; justify-content: space-between; align-items: center; padding: 0 40px">
+    <img src="logo.svg" alt="Moscow Polytech" height="56"/>
+    <h1>Задание: Hello, World!</h1>
+</header>
+<main>
+    <?php echo '<p>Hello World</p>'; ?>
+</main>
+<footer>
+    <hr/>
+    <p>
+        Copyright <?php echo date("Y") ?>
+
+    </p>
+</footer>
+</body>
+</html>
